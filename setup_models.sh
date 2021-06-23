382382db8a4ab32c50560dc67da92b4163d488ef
rishatik92 <Rishatik92@gmail.com>
2021-06-24 00:04:38 +0300

    fix deploy script

--- setup_models.sh
+++ setup_models.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 wget -O models_ru.zip 'https://nomeroff.net.ua/datasets/autoriaNumberplateOcrRu-2020-10-12.zip'
 unzip -d models/ models_ru.zip
+mv models/autoriaNumberplateOcrRu-*/*  models/
+rm -r models/autoriaNumberplateOcrRu*
 rm models_ru.zip
